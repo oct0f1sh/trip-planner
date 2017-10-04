db985094bdd77a91c109562efd3037c403a792a9
Duncan <buzzbuzz.macdonald@gmail.com>
2017-10-04 15:55:13 -0700

    ??
    i don't know

--- server.py
+++ server.py
@@ -8,16 +8,34 @@ import bcrypt
 
 app = Flask(__name__)
 mongo = MongoClient('localhost', 27017)
-app.db = mongo.trip_planner_development
+app.db = mongo.local
 app.bcrypt_rounds = 12
 api = Api(app)
 
 
-## Write Resources here
+# Write Resources here
+class User(Resource):
+    def post(self):
+        new_myobject = request.json
+        myobject_collection = app.db.users
+        result = myobject_collection.insert_one(new_myobject)
+        myobject = myobject_collection.find_one({"_id": ObjectId(result.inserted_id)})
+        return myobject
+
+    def get(self, myobject_id):
+        myobject_collection = app.db.users
+        myobject = myobject_collection.find_one({"_id": ObjectId(myobject_id)})
+
+        if myobject is None:
+            response = "error"
+            return (response, 404, None)
+        else:
+            return myobject
 
 
+#  Add api routes here
+api.add_resource(User, '/users')
 
-## Add api routes here
 
 #  Custom JSON serializer for flask_restful
 @api.representation('application/json')
@@ -25,6 +43,7 @@ def output_json(data, code, headers=None):
     resp = make_response(JSONEncoder().encode(data), code)
     resp.headers.extend(headers or {})
     return resp
+
 
 if __name__ == '__main__':
     # Turn this on in debug mode to get detailled information about request
--- tests.py
+++ tests.py
@@ -8,26 +8,31 @@ from pymongo import MongoClient
 
 class TripPlannerTestCase(unittest.TestCase):
     def setUp(self):
+        self.app = server2.app.test_client()
+        server2.app.config['TESTING'] = True
+        mongo = MongoClient('localhost', 27107)
+        global db
+        db = mongo.local
+        server2.app.db = db
+        db.drop_collection('users')
 
-      self.app = server.app.test_client()
-      # Run app in testing mode to retrieve exceptions and stack traces
-      server.app.config['TESTING'] = True
-
-      mongo = MongoClient('localhost', 27017)
-      global db
-
-      # Reduce encryption workloads for tests
-      server.app.bcrypt_rounds = 4
-
-      db = mongo.trip_planner_test
-      server.app.db = db
-
-      db.drop_collection('users')
-      db.drop_collection('trips')
-
-    # User tests, fill with test methods
     def testCreateUser(self):
-        pass
+        self.app.post(
+            '/users',
+            data=json.dumps(dict(
+            password="test",
+            email="dunc@dunc.co"
+            )), content_type='application/json'
+        )
+
+        response = self.app.get(
+            '/users',
+            query_string=dict(email="dunc@dunc.co")
+        )
+
+        response_json = json.loads(response.data.decode())
+
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
